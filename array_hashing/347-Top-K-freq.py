045593f5244548209972907bd5f5c6e604722030
ananthanandanan <ananthanandanan@gmail.com>
2023-09-09 13:16:50 +0530

    array hasing 347

--- array_hashing/347-Top-K-freq.py
+++ array_hashing/347-Top-K-freq.py
@@ -2,7 +2,9 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         res = []
         count = {}
-        freq = [[] for i in range(len(nums) + 1)]
+        ## the case were one number appears all the time.
+
+        freq = [[] for i in range(len(nums) + 1)]  
 
         for i in nums:
             count[i] = 1 + count.get(i, 0)
