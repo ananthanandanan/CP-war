def4d18063f2f2a602c845426d34a96f1137f2c9
ananthanandanan <ananthanandanan@gmail.com>
2024-07-20 17:18:39 +0530

    commit one

--- pydantic_tests/pydantic_test.py
+++ pydantic_tests/pydantic_test.py
@@ -4,8 +4,9 @@ import os
 class AzureOpenAISettings(BaseSettings):
     AZURE_OPENAI_API_KEY: str
     AZURE_OPENAI_ENDPOINT: str
-    OPENAI_API_VERSION: str = "2023-05-15"
+    OPENAI_API_VERSION: str = "2023-07-01"  # Updated API version
     AZURE_OPENAI_DEPLOYMENT: str
+    AZURE_OPENAI_MODEL: str  # Added new field
 
     class Config:
         env_file = None
